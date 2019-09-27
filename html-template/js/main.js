dbad05c7535b68d90a62d3c8ca7a829154f31e34
Tobias Sundgren <Tobias.Sundgren@collectorbank.se>
2019-09-27 09:36:36 +0200

    Styling

--- html-template/js/main.js
+++ html-template/js/main.js
@@ -1,1 +1,26 @@
     
+/* Mobile Navigation
+    -----------------------------------------------
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+*/
+
+
+/* HTML document is loaded. DOM is ready. 
+-------------------------------------------*/
+$(document).ready(function() {
+
+    svg4everybody(); 
+    /* Hide mobile menu after clicking on a link
+      -----------------------------------------------*/
+      $('.navbar-collapse a').click(function(){
+          $(".navbar-collapse").collapse('hide');
+      });
+    }
+);
+  
